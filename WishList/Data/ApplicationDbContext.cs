41739f0b913a91ebf078b26f35ce348e78464c2b
June Panic <june_panic@yahoo.com>
2018-07-08 23:26:15 -0500

    Add ApplicationDbContext

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace WishList.Data
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly RequestDelegate _next;
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
